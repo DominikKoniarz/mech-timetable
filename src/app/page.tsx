5becc0eae6634e4a56cb2dbdbbdbefa3a00229ab
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-03-10 20:47:39 +0100

    Add theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
+import { useTheme } from "next-themes";
+
 export default function Home() {
+	const { setTheme } = useTheme();
+
+	const toggleTheme = () => {
+		setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+	};
 	return (
-		<main className="w-full h-full bg-[#F8F9FA]">
-			<h1 className="text-[#212529] font-bold text-4xl">
-				Welcome to Mech Timetable
-			</h1>
+		<main className="w-full h-full bg-background text-foreground">
+			<h1 className="font-bold text-4xl">Welcome to Mech Timetable</h1>
 			<p>foo bar</p>
+			<button onClick={toggleTheme}>Toggle Theme</button>
+			{/* set system */}
+			<button onClick={() => setTheme("system")}>Set System</button>
 		</main>
 	);
 }
