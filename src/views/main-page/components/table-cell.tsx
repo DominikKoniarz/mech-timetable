e10a2a5b17ff7714dfe0c9ad2b4758d386bd2455
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-05-01 23:00:50 +0200

    Add icons

--- src/views/main-page/components/table-cell.tsx
+++ src/views/main-page/components/table-cell.tsx
@@ -1,5 +1,6 @@
 import type { ClassEntry } from "@/types/classes";
 import { checkCurrentWeekParity } from "@/lib/data/helpers";
+import { MdOutlinePlace, MdOutlineClass } from "react-icons/md";
 
 type Props = {
 	classEntries: ClassEntry[];
@@ -8,15 +9,24 @@ type Props = {
 export default function TableCell({ classEntries }: Props) {
 	const parity = checkCurrentWeekParity();
 
+	// Find the class entry that matches the current parity or has no parity
 	const foundClassEntry = classEntries.find(
 		(classItem) => classItem.parity === parity || classItem.parity === null
 	);
 
 	return (
 		<td className="px-4 py-2">
-			<div className="h-24 bg-card rounded-lg">
-				{foundClassEntry &&
-					`${foundClassEntry.subject} ${foundClassEntry.room}`}
+			<div className="h-24 bg-card rounded-lg flex flex-col justify-center items-center gap-2">
+				{foundClassEntry && foundClassEntry.classType !== "LECTURE" && (
+					<>
+						<div className="flex flex-row items-center gap-1 text-base">
+							<MdOutlineClass className="text-xl" /> {foundClassEntry.subject}
+						</div>
+						<div className="flex flex-row items-center gap-1 text-base">
+							<MdOutlinePlace className="text-xl" /> {foundClassEntry.room}
+						</div>
+					</>
+				)}
 			</div>
 		</td>
 	);
