3a324118a1e4100d8ca188802b036b062bc73b6f
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-05-03 21:13:29 +0200

    Cache loading messages for infinite amount of time

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,25 +1,36 @@
 import { hasLocale } from "next-intl";
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
+import { unstable_cache } from "next/cache";
+
+const getMessages = unstable_cache(
+    async (locale: string) => {
+        let messages;
+
+        try {
+            messages = (await import(`../../messages/${locale}.json`)).default;
+        } catch {
+            console.error(`No messages found for locale: ${locale}`);
+            messages = {};
+        }
+
+        return messages;
+    },
+    undefined,
+    { revalidate: undefined },
+);
 
 export default getRequestConfig(async ({ requestLocale }) => {
-	const requested = await requestLocale;
-	const locale = hasLocale(routing.locales, requested)
-		? requested
-		: routing.defaultLocale;
+    const requested = await requestLocale;
+    const locale = hasLocale(routing.locales, requested)
+        ? requested
+        : routing.defaultLocale;
 
-	let messages;
+    const messages = await getMessages(locale);
 
-	try {
-		messages = (await import(`../../messages/${locale}.json`)).default;
-	} catch {
-		console.error(`No messages found for locale: ${locale}`);
-		messages = {};
-	}
-
-	return {
-		locale,
-		messages,
-		formats: null,
-	};
+    return {
+        locale,
+        messages,
+        formats: null,
+    };
 });
