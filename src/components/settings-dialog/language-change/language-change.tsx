170650cc82c70d3839387757698e00261bed44ac
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-05-22 18:21:04 +0200

    Modify i18n message

--- src/components/settings-dialog/language-change/language-change.tsx
+++ src/components/settings-dialog/language-change/language-change.tsx
@@ -8,7 +8,12 @@ export default function LanguageChange() {
 
     return (
         <div className="flex flex-col gap-1">
-            <h3 className="mt-3 text-left text-sm">{t("changeLanguage")}</h3>
+            <h3 className="mt-3 text-left text-sm">
+                {t("changeLanguage")}{" "}
+                <span className="lowercase italic">
+                    ({t("pageWillReload")})
+                </span>
+            </h3>
             <div className="mt-0.5 flex flex-row gap-2">
                 <ChangeLanguageButton localeToSet="pl">
                     <FlagPL />
