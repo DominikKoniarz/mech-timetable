7069f0a43361fb0dd9cfd62af627b828709b880b
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-09-23 20:51:48 +0200

    Add desktop week switch button

--- src/views/main-page/components/table-head.tsx
+++ src/views/main-page/components/table-head.tsx
@@ -1,7 +1,15 @@
+"use client";
+
 import { useTranslations } from "next-intl";
+import { useMainPageStore } from "../context/main-page-provider";
+import { Button } from "@/components/ui/button";
 
 export default function TableHead() {
     const t = useTranslations("mainPage.table.tableHead");
+
+    const { displayNextWeek, toggleDisplayNextWeek } = useMainPageStore(
+        (state) => state,
+    );
 
     const weekdays: string[] = [
         t("monday"),
@@ -18,8 +26,14 @@ export default function TableHead() {
                     scope="col"
                     className="text-foreground w-32 px-4 py-2 text-xs font-medium tracking-wider uppercase"
                 >
-                    {/* sticky top-0 z-10 */}
-                    {t("hour")}
+                    <Button
+                        type="button"
+                        onClick={toggleDisplayNextWeek}
+                        className="w-full cursor-pointer"
+                        variant="outline"
+                    >
+                        {displayNextWeek ? t("next") : t("current")}
+                    </Button>
                 </th>
                 {weekdays.map((day, index) => (
                     <th
