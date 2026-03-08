ddca31b3cf8045e756de0b1800c9403b8cb2ec04
DominikKoniarz <dominik.koniarz.development@gmail.com>
2026-03-08 22:30:27 +0100

    Try to fix VERCEL_URL env

--- src/lib/eden-client.ts
+++ src/lib/eden-client.ts
@@ -1,8 +1,9 @@
 import type { App } from "@/app/[locale]/api/[[...slugs]]/route";
+import { env } from "@/env";
 import { treaty } from "@elysiajs/eden";
 
 export const client = treaty<App>(
-    process.env.VERCEL_URL
-        ? `${process.env.VERCEL_URL}`
+    env.NEXT_PUBLIC_VERCEL_URL
+        ? `${env.NEXT_PUBLIC_VERCEL_URL}`
         : "http://localhost:3000", // TODO: use typesafe env variable
 );
